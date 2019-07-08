a83a5e3549024160f78834c470d77b647d75488c
Slav666 <slawomirdyk@gmail.com>
2019-07-09 00:04:46 +0100

    Reset button works

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,47 +1,36 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {
-        value: this.props.value
-        
-    };
-  
-        handleIncrement = () => {
+    render() { 
             
-            this.setState({count: this.state.value + 1});
-
-        };
-
-        
-        render() { 
-            console.log(this.props);
             return (
                 <div>
                    
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button 
-                    onClick={ this.handleIncrement }
+                    onClick={ () => this.props.onIncrement(this.props.counter) }
                     className="btn btn-secondary btn-sm"
                     >
                     Increment
                 </button>
-                <button onClick={() => this.props.onDelete(this.props.id)}
+                <button onClick={() => this.props.onDelete(this.props.counter.id)}
                 className="btn btn-danger btn-sm m-2">
                     Delete
                     </button>
-            
+                    
                 </div>
             );
         }
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.value === 0 ? "warning" : "primary";
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-       return this.state.value === 0 ? 'Zero' : this.state.value;
+        const {value} = this.props.counter;
+       return value === 0 ? 'Zero' : value;
     }
 }
  
